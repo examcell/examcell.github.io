f511da43872c12ca9c9693c826a6fd99eca2a1da
Bhargav Reddi <bhargav@localhost>
2017-03-22 08:11:34 +0530

    Arrangement Module

--- js/site.js
+++ js/site.js
@@ -2,6 +2,12 @@
 url1 = 'http://bhargavreddi.pythonanywhere.com';
 url2 = 'http://127.0.0.1:8000';
 url_link = url2;
+exam_id = null;
+time = null;
+date = null;
+capacity = 0;
+dhcapacity = 0;
+classroom_count = 0;
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -53,7 +59,7 @@ function getRegulations()
 			});	
                     });
 }
-function sendFile()
+function sendRegisterStudents()
 {
 	var data = new FormData($('form#upload').get(0));
 	$.ajax({
@@ -64,115 +70,321 @@ function sendFile()
     	processData: false,
     	contentType: false,
     	success: function(data) {
-        alert('success');
+	if(data.status == 'success')
+		alert('success');
+	else
+		alert('failed');
+        window.opener = self;
+        window.close();
     	}
 });
 }
-function sendData(image_id,user_id,user_name) {
-    addHeader();
-	$.ajax({
-		url: url_link+'/api/images/',
-		type: 'POST',
-		contentType: 'application/json',
-		data: JSON.stringify({
 
-			image_id:image_id,
-			user_id : user_id
-		}),
-		dataType: 'json'
-	});
-	var color = $(".glyphicon-thumbs-up").css("color");
-	if(color=="rgb(0, 0, 255)")
-	{
-		$(".glyphicon-thumbs-up").css("color","black");
-		var x =parseInt($("#json").html());
-		$("#json").html(x-1);
-		$("#user"+user_id).remove();
-	}
-	else {
-		$(".glyphicon-thumbs-up").css("color", "blue");
-		var x =parseInt($("#json").html());
-		$("#json").html(x+1);
-		$('#user_liked').append(
-			"<a href=\"/profile/"+user_id+"\" id=\"user"+user_id+"\">"+"<li class=\"list-group-item\"><h3 align=\"left\">" + user_name + "</h3></li></a>"
-		);
-	}
-}
-function clickComment(image_id,user_id,nameUser){
-	addHeader();
-	var text = $("#comment").val();
-	var comment_id = "";
-	$.ajax({
-		url: url_link+'/api/comments/',
-		type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-
-            image_id:image_id,
-			user_id : user_id,
-            comment : text
-        }),
-		success:function (data) {
-			comment_id = data;
-			var division = "<div class=\"alert alert-success alert-dismissible\" role=\"alert\">";
-			var button = "<button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\" onclick=\"deleteComment("+comment_id+")\"><span aria-hidden=\"true\">&times;</span></button>";
-			var data = "<p><strong>"+nameUser+"</strong></p>"+text+"</div>";
-			$('#comments').append(
-				division+button+data
-			);
-		},
-        dataType: 'json'
-    });
-    $('#comment').val('');
-}
-function search()
+function sendStudentList()
 {
-	var context = $("#search_user").val();
-	if(context =="" || context == null)
-		return;
-	window.location.replace(url_link+"/search/"+context+"/");
+	var data = new FormData($('form#upload').get(0));
+	$.ajax({
+    	url: url_link + "/api/upload_student_list/",
+    	type: 'POST',
+    	data: data,
+    	cache: false,
+    	processData: false,
+    	contentType: 'false',
+    	success: function(data) {
+	if(data.status == 'success')
+		alert('success');
+	else
+		alert('failed');
+        window.opener = self;
+        window.close();
+    	}
+});
 }
-function follow(user_id,follower_id) {
-	if(user_id == follower_id)
+
+function sendTimetable()
+{
+	var data = new FormData($('form#upload').get(0));
+	$.ajax({
+    	url: url_link + "/api/upload_timetable/",
+    	type: 'POST',
+    	data: data,
+    	cache: false,
+    	processData: false,
+    	contentType: false,
+    	success: function(data) {
+	if(data.status == 'success')
+		alert('success');
+	else
+		alert('failed');
+        window.opener = self;
+        window.close();
+    	}
+});
+}
+
+function sendFacultyTimetable()
+{
+	var data = new FormData($('form#upload').get(0));
+	$.ajax({
+    	url: url_link + "/api/upload_faculty_timetable/",
+    	type: 'POST',
+    	data: data,
+    	cache: false,
+    	processData: false,
+    	contentType: false,
+    	success: function(data) {
+	if(data.status == 'success')
+		alert('success');
+	else
+		alert('failed');
+        window.opener = self;
+        window.close();
+    	}
+});
+}
+function setExamId(id)
+{
+	exam_id = id;
+}
+
+function removeTimetable()
+{
+	if (exam_id == null)
 	{
-		alert("You Cannot Follow yourself");
+		alert('Select Examination');
 		return;
 	}
-	addHeader();
-	$.ajax({
-		url: url_link+'/api/follow/',
-		type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
+	$.getJSON(url_link+"/api/delete_timetable/"+exam_id,
+                    function (data) {
+				if(data.status == 'success')
+					alert('success');
+				else
+					alert('failed');
+            			window.opener = self;
+        			window.close();
+			});	
+	
+}
 
-            follow_id:follower_id,
-			user_id : user_id
-        }),
-        dataType: 'json'
-    });
-	if($("#follow_button").html() == 'Follow')
+function removeRegisterStudents()
+{
+	if (exam_id == null)
 	{
-		$("#follow").html(
-			"<button type=\"button\"  class=\"btn btn-warning btn-lg\" onclick=\"follow('{{ user.id }}','{{ profile_id }}')\" id=\"follow_button\">Unfollow</button> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;"
-		);
+		alert('Select Examination');
+		return;
+	}
+	$.getJSON(url_link+"/api/delete_register_students/"+exam_id,
+                    function (data) {
+				if(data.status == 'success')
+					alert('success');
+				else
+					alert('failed');
+            			window.opener = self;
+			        window.close();
+			});	
+}
+
+
+function getExaminationTiming(id)
+{
+	exam_id = id;
+	$.getJSON(url_link+"/api/getTimings/" + id + "/",
+                    function (data) {
+			$.each(data.time_list, function (index, element) {
+			$('#time').append("<li><a href=\"javascript:settime('"+element.time+"')\">"  + element.time  +"</a></li>");
+			});
+                    });
+}
+function settime(t)
+{
+	time = t;
+}
+function checkExam()
+{
+	if(exam_id == null)
+	{
+		alert('Select Examination');
+		return;
+	}
+	if(time == null)
+	{
+		alert('Select Time');
+		return;
+	}
+	date = document.getElementById('datepicker').value;
+	if(date == null || date == '')
+	{
+		alert('Select Date');
+		return;
+	}
+	date_array = date.split("/");
+	$.getJSON(url_link+"/api/checkExam/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/",
+                    function (data) {
+			if (data.status == 'true')
+			window.location = "view_classroom.html?exam_id="+exam_id+"&date="+date+"&time="+time;
+			else
+			alert('No Exams Scheduled');
+                    });
+}
+function getCapacity(exam_id,date,time)
+{
+	date_array = date.split("/");
+	time_array = time.split(":");
+	link = url_link+"/api/getCapacity/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1] + "/";
+        $.getJSON(link,function (data) {
+			capacity = data.capacity;
+			dhcapacity = data.dhcapacity;
+			$("#capacity").text(capacity);
+			$("#dhcapacity").text(dhcapacity);
+                    });
+	
+}
+function sendClassrooms(exam_id,date,time)
+{
+	selected_capacity = 0;
+	selected_dhcapacity = 0;
+	classroom_list = new Array();
+	drawinghall_list = new Array();
+	index = 0;
+	dindex = 0;
+	for(i=1;i<=classroom_count;i++)
+	{
+		if($("#"+i).is(":checked")){
+		    //do something
+		  
+		    size = parseInt($("#"+i).val());
+		    if(size == 56 && (dhcapacity - selected_dhcapacity) > 0)
+		    {
+			drawinghall_list[dindex++] =  $("#"+i).text();
+			selected_dhcapacity += size;
+		    }
+		    else
+			if(selected_capacity < capacity)
+			{
+		    	selected_capacity += size;
+			classroom_list[index++] =  $("#lb"+i).text();
+			}
+		    if (selected_capacity >= capacity && selected_dhcapacity >= dhcapacity)
+			break;
+		}
+	}
+	$("#capacity").text((capacity-selected_capacity));
+        $("#dhcapacity").text((dhcapacity-selected_dhcapacity));
+	if (selected_capacity >= capacity && selected_dhcapacity >= dhcapacity)
+	{
+		obj = new Object();
+		obj.classrooms = classroom_list;
+		obj.drawinghalls = drawinghall_list;
+		size = index;
+		data = JSON.stringify(obj);
+		date_array = date.split("/");
+		time_array = time.split(":");
+		link = url_link+"/api/arrangeStudents/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1] + "/";
+		$.ajax({
+		url: link,
+		type: 'POST',
+		data: data,
+		dataType: "json",
+		contentType: "application/json",
+		success: function(data) {
+		if(data.status == 'success')
+			window.location = "view_menu.html?exam_id="+exam_id+"&date="+date+"&time="+time;
+		else
+			alert('failed');
+		}
+		});
 	}
 	else
-	{
-		$("#follow").html(
-			"<button type=\"button\"  class=\"btn btn-success btn-lg\" onclick=\"follow('{{ user.id }}','{{ profile_id }}')\" id=\"follow_button\">Follow</button> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;"
-		);
-	}
+		alert('Select More Classrooms');
 }
-function deleteComment(id)
+function getClassrooms(exam_id,date,time)
 {
-	addHeader();
-	$.ajax({
-		url: url_link+'/api/comment/delete/',
-		type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify({
-            comment_id:id
-        }),
-        dataType: 'json'
-    });
+	date_array = date.split("/");
+	time_array = time.split(":");
+	link = url_link+"/api/getClassrooms/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1] + "/";
+	$.getJSON(link,
+                    function (data) {
+			classroom_count = data.number;
+			count = 1
+			$.each(data.classroom_list, function (index, element) {
+			$('#cls_list').append("<div class=\"checkbox\">&nbsp; &nbsp;<h3><input type=\"checkbox\" id = \""+count+"\" value=\""+element.size+"\"><label id=\"lb"+count+"\">"+element.classroom+"</label></h3></div>");
+			count++;
+			});
+			$('#cls_list').append("<br><br><div><button type=\"button\" class=\"btn btn-success\" id=\"submit\" onClick=\"sendClassrooms(exam_id,date,time);\">Next</button></div><br><br>");
+                    });
+}
+function retrieveClassrooms(exam_id,date,time)
+{
+	date_array = date.split("/");
+	time_array = time.split(":");
+	link = url_link+"/api/retreiveClassrooms/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1] + "/";
+	    $.getJSON(link,
+                    function (data) {
+			$.each(data.classroom_list, function (index, element) {
+				$('#exams').append("<li><a href=\"javascript:generateHallPlan('"+element.size+"','"+element.classroom+"')\">" + element.classroom +"</a></li>");
+			});	
+                    });
+}
+function room36(classroom)
+{
+	student_list = new Array();
+	for(i=0;i<6;i++)
+	 student_list[i] = new Array();
+	date_array = date.split("/");
+	time_array = time.split(":");
+	link = url_link+"/api/studentsClassroom/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1] + "/" + classroom + "/" ;
+	    $.getJSON(link,
+                    function (data) {
+			index_1 = 0;
+			
+			index_2 = 0;
+			$.each(data.student_list, function (index, element) {
+				regno = element.regno;
+				student_list[index_1][index_2] = regno;
+				index_1 = (index_1 + 1)%6;
+				if(index_1 == 0)
+					index_2++;
+			});	
+			while(index_1 == 6)
+				student_list[index_1++][index_2] = ' ';
+			print_script = "<table width=\"100%\">";
+			for(i=0;i<6;i++)
+				print_script += "<tr><td>"+student_list[i][0]+"</td><td>"+student_list[i][1]+"</td><td>"+student_list[i][2]+"</td><td>"+student_list[i][3]+"</td><td>"+student_list[i][4]+"</td><td>"+student_list[i][5]+"</td></tr>";
+			print_script += "</table>";
+			$('#display').html(print_script);
+
+                    });
+}
+function isArranged()
+{
+	if(exam_id == null)
+	{
+		alert('Select Examination');
+		return;
+	}
+	if(time == null)
+	{
+		alert('Select Time');
+		return;
+	}
+	date = document.getElementById('datepicker').value;
+	if(date == null || date == '')
+	{
+		alert('Select Date');
+		return;
+	}
+	date_array = date.split("/");
+	time_array = time.split(":");
+	$.getJSON(url_link+"/api/isArranged/" + exam_id + "/" + date_array[2] + "/" + date_array[0] + "/" + date_array[1] + "/" + time_array[0] + "/" + time_array[1],
+                    function (data) {
+			if (data.status == 'true')
+			window.location = "view_menu.html?exam_id="+exam_id+"&date="+date+"&time="+time;
+			else
+			alert('No Exams Scheduled');
+                    });
+}
+function generateHallPlan(size,classroom)
+{
+	alert(size);
+	room36(classroom);
 }
